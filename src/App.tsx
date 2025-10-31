729a807171acab57ec46883d0b134aec454732a8
SigmaElite <tvojlubimyjcel@gmail.com>
2025-10-31 20:04:26 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HotelPage from './pages/HotelPage';
 import RestaurantPage from './pages/RestaurantPage';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen">
       <Header />
